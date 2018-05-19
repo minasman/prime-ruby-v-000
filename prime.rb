ec959ab42ce640ab9f0bb1afc745376ce454cc42
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 21:17:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,3 @@
-
-
-Benchmark.bm do|bm|
-
-bm.report do
-  
 def prime?(int)
   return false if int < 2 
   return true if int == 3 || int == 2 
@@ -13,9 +7,3 @@ def prime?(int)
     true
   end
 end
-
-
-end #bm.report
-end #Benchmark
-
-#prime?(4)
