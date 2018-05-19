46680fe118de033e632002a8d8e798891dca478e
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 21:10:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+require 'benchmark'
+require 'pry'
+
+Benchmark.bm do|bm|
+
+bm.report do
+  
+def prime?(int)
+  return false if int <= 1 
+  if int == 2 || int == 3 || int == 5 || int == 7 || int == 11 
+    return TRUE
+  else 
+    num = 2
+    while num <= int
+      if int % num == 0 
+        return false
+      end
+      num += 1
+    end
+    return true
+  end
+end
+
+
+end #bm.report
+end #Benchmark
+
+#prime?(4)
