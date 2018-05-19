a3917cf3df8303b5783361a5e96fde6ed4c55921
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 21:10:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,18 +7,7 @@ bm.report do
   
 def prime?(int)
   return false if int <= 1 
-  if int == 2 || int == 3 || int == 5 || int == 7 || int == 11 
-    return TRUE
-  else 
-    num = 2
-    while num <= int
-      if int % num == 0 
-        return false
-      end
-      num += 1
-    end
-    return true
-  end
+
 end
 
 
