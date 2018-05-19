3df168724a441a06b564f247566f1310e0a6ee8f
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 21:12:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,13 @@ Benchmark.bm do|bm|
 bm.report do
   
 def prime?(int)
-  return false if int <= 1 
-
+  return false if int < 2 
+  return true if int == 3 || int == 2 
+  if (2...int-1).any?{|i| int % i == 0}
+    false
+  else
+    true
+  end
 end
 
 
